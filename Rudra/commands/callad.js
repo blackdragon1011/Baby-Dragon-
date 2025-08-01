b12b357b92696e0f702df7ee3075d6d91f988407
Tamimkot2324 <mdsaim232456@gmail.com>
2025-08-01 10:36:26 +0600

    Update callad.js

--- Rudra/commands/callad.js
+++ Rudra/commands/callad.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "callad",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝐌𝐝 𝐓𝐚𝐦𝐢𝐦",
   description: "Report bug of your bot to admin or comment",
   commandCategory: "Admin",
   usages: "[msg]",
@@ -23,35 +23,23 @@ module.exports.handleReply = async function({ api, args, event, handleReply, Use
       for (var p of event.attachments) {
         var result = '';
         for (var i = 0; i < charactersLength; i++) result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        if (p.type == 'photo') {
-          var e = 'jpg';
-        }
-        if (p.type == 'video') {
-          var e = 'mp4';
-        }
-        if (p.type == 'audio') {
-          var e = 'mp3';
-        }
-        if (p.type == 'animated_image') {
-          var e = 'gif';
-        }
+        var e = p.type === 'photo' ? 'jpg' : p.type === 'video' ? 'mp4' : p.type === 'audio' ? 'mp3' : 'gif';
         var o = join(__dirname, 'cache', `${result}.${e}`);
         let m = (await axios.get(encodeURI(p.url), { responseType: "arraybuffer" })).data;
         fs.writeFileSync(o, Buffer.from(m, "utf-8"));
         s.push(o);
         l.push(fs.createReadStream(o));
       }
-    };
+    }
+
     switch (handleReply.type) {
       case "reply": {
         var idad = global.config.ADMINBOT;
         if (s.length == 0) {
           for (let ad of idad) {
             api.sendMessage({
-              body: "[📲] Feedback from " + name + " :\n[💬] Content: " + (event.body) || "There's no answer", mentions: [{
-                id: event.senderID,
-                tag: name
-              }]
+              body: "[📲] Feedback from " + name + " :\n[💬] Content: " + (event.body || "There's no answer"),
+              mentions: [{ id: event.senderID, tag: name }]
             }, ad, (e, data) => global.client.handleReply.push({
               name: this.config.name,
               messageID: data.messageID,
@@ -61,14 +49,12 @@ module.exports.handleReply = async function({ api, args, event, handleReply, Use
               type: "calladmin"
             }));
           }
-        }
-        else {
+        } else {
           for (let ad of idad) {
             api.sendMessage({
-              body: "[📲] Feedback from " + name + ":\n" + (event.body) || "only files that don't have a reply 🧡", attachment: l, mentions: [{
-                id: event.senderID,
-                tag: name
-              }]
+              body: "[📲] Feedback from " + name + ":\n" + (event.body || "only files that don't have a reply 🧡"),
+              attachment: l,
+              mentions: [{ id: event.senderID, tag: name }]
             }, ad, (e, data) => global.client.handleReply.push({
               name: this.config.name,
               messageID: data.messageID,
@@ -77,38 +63,40 @@ module.exports.handleReply = async function({ api, args, event, handleReply, Use
               id: event.threadID,
               type: "calladmin"
             }));
-            for (var b of s) {
-              fs.unlinkSync(b);
-            }
+            for (var b of s) fs.unlinkSync(b);
           }
         }
         break;
       }
+
       case "calladmin": {
         if (s.length == 0) {
-          api.sendMessage({ body: `[📌] Feedback from admin ${name} to you:\n\n[💬] Content: ${(event.body) || "no reply 🌸 "}\n\n» Reply to this message if you want to continue sending  reports to my owner 𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭`, mentions: [{ tag: name, id: event.senderID }] }, handleReply.id, (e, data) => global.client.handleReply.push({
+          api.sendMessage({
+            body: `[📌] Feedback from admin ${name} to you:\n\n[💬] Content: ${(event.body || "no reply 🌸 ")}\n\n» Reply to this message if you want to continue sending reports to my owner 𝐌𝐝 𝐓𝐚𝐦𝐢𝐦`,
+            mentions: [{ tag: name, id: event.senderID }]
+          }, handleReply.id, (e, data) => global.client.handleReply.push({
             name: this.config.name,
             author: event.senderID,
             messageID: data.messageID,
             type: "reply"
           }), handleReply.messID);
-        }
-        else {
-          api.sendMessage({ body: `[📌] Feedback from admin ${name} to you:\n\n[💬] Content: ${(event.body) || "only files with no reply 🌸 "}\n[💌] Admin files sent to you\n\n» Reply to this message if you want to continue sending reports to my owner 𝐌𝐝 𝐓𝐚𝐦𝐢𝐦`, attachment: l, mentions: [{ tag: name, id: event.senderID }] }, handleReply.id, (e, data) => global.client.handleReply.push({
+        } else {
+          api.sendMessage({
+            body: `[📌] Feedback from admin ${name} to you:\n\n[💬] Content: ${(event.body || "only files with no reply 🌸 ")}\n[💌] Admin files sent to you\n\n» Reply to this message if you want to continue sending reports to my owner 𝐌𝐝 𝐓𝐚𝐦𝐢𝐦`,
+            attachment: l,
+            mentions: [{ tag: name, id: event.senderID }]
+          }, handleReply.id, (e, data) => global.client.handleReply.push({
             name: this.config.name,
             author: event.senderID,
             messageID: data.messageID,
             type: "reply"
           }), handleReply.messID);
-          for (var b of s) {
-            fs.unlinkSync(b);
-          }
+          for (var b of s) fs.unlinkSync(b);
         }
         break;
       }
     }
-  }
-  catch (ex) {
+  } catch (ex) {
     console.log(ex);
   }
 };
@@ -122,23 +110,11 @@ module.exports.run = async function({ api, event, Threads, args, Users }) {
     const axios = require('axios');
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     var charactersLength = characters.length || 20;
-    if (event.messageReply) {
-    if (event.messageReply.attachments.length != 0) {
+    if (event.messageReply && event.messageReply.attachments.length != 0) {
       for (var p of event.messageReply.attachments) {
         var result = '';
         for (var i = 0; i < charactersLength; i++) result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        if (p.type == 'photo') {
-          var e = 'jpg';
-        }
-        if (p.type == 'video') {
-          var e = 'mp4';
-        }
-        if (p.type == 'audio') {
-          var e = 'mp3';
-        }
-        if (p.type == 'animated_image') {
-          var e = 'gif';
-        }
+        var e = p.type === 'photo' ? 'jpg' : p.type === 'video' ? 'mp4' : p.type === 'audio' ? 'mp3' : 'gif';
         var o = join(__dirname, 'cache', `${result}.${e}`);
         let m = (await axios.get(encodeURI(p.url), { responseType: "arraybuffer" })).data;
         fs.writeFileSync(o, Buffer.from(m, "utf-8"));
@@ -146,16 +122,12 @@ module.exports.run = async function({ api, event, Threads, args, Users }) {
         l.push(fs.createReadStream(o));
       }
     }
-  }
-    if (!args[0] && event.messageReply.attachments.length == 0)
-      return api.sendMessage(`You haven't entered what to report 📋`,
-        event.threadID,
-        event.messageID
-      );
+
+    if (!args[0] && s.length === 0)
+      return api.sendMessage(`You haven't entered what to report 📋`, event.threadID, event.messageID);
 
     var name = (await Users.getData(event.senderID)).name;
     var idbox = event.threadID;
-
     var datathread = (await Threads.getData(event.threadID)).threadInfo;
     var namethread = datathread.threadName;
     var uid = event.senderID;
@@ -163,53 +135,42 @@ module.exports.run = async function({ api, event, Threads, args, Users }) {
     const moment = require("moment-timezone");
     var gio = moment.tz("Asia/Manila").format("HH:mm:ss D/MM/YYYY");
     var soad = global.config.ADMINBOT.length;
-    api.sendMessage(`[🤖] - Bot has just successfully sent your message to ${soad} to my owner 𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭 🍄 \n[⏰] - Time: ${gio}`,
-      event.threadID,
-      () => {
-        var idad = global.config.ADMINBOT;
-        if (s.length == 0) {
-          for (let ad of idad) {
-            api.sendMessage({ body: `📱[ CALL ADMIN ]📱 \n\n\n[👤] Report from: ${name}\n[❗] ID User ${uid}\n[🗣️] BOX: ${namethread}\n[🔰] ID BOX: ${idbox}\n\n[💌] Inbox: ${args.join(
-              " "
-            )}\n[⏰] Time: ${gio}`, mentions: [{ id: event.senderID, tag: name }] },
-              ad, (error, info) =>
-              global.client.handleReply.push({
-                name: this.config.name,
-                messageID: info.messageID,
-                author: event.senderID,
-                messID: event.messageID,
-                id: idbox,
-                type: "calladmin"
-              })
-            );
-          }
+
+    api.sendMessage(`[🤖] - Bot has just successfully sent your message to ${soad} to my owner 𝐌𝐝 𝐓𝐚𝐦𝐢𝐦 🍄 \n[⏰] - Time: ${gio}`, event.threadID, () => {
+      var idad = global.config.ADMINBOT;
+      if (s.length == 0) {
+        for (let ad of idad) {
+          api.sendMessage({
+            body: `📱[ CALL ADMIN ]📱 \n\n\n[👤] Report from: ${name}\n[❗] ID User ${uid}\n[🗣️] BOX: ${namethread}\n[🔰] ID BOX: ${idbox}\n\n[💌] Inbox: ${args.join(" ")}\n[⏰] Time: ${gio}`,
+            mentions: [{ id: event.senderID, tag: name }]
+          }, ad, (error, info) => global.client.handleReply.push({
+            name: this.config.name,
+            messageID: info.messageID,
+            author: event.senderID,
+            messID: event.messageID,
+            id: idbox,
+            type: "calladmin"
+          }));
         }
-        else {
-          for (let ad of idad) {
-            api.sendMessage({
-              body: `📱 ===[CALL ADMIN]====📱\n\n\n[👤] Report from: ${name}\n[❗] ID User ${uid}\n[ 👧 👩 👧 👨 ] BOX: ${namethread}\n[🔰] ID BOX: ${idbox}\n\n[💌] Inbox: ${(args.join(
-                " "
-              )) || "only files that do not have report ❤️ content"}\n[⏰] Time: ${gio}\n[📌] Attach a file`, attachment: l, mentions: [{ id: event.senderID, tag: name }]
-            },
-              ad, (error, info) =>
-              global.client.handleReply.push({
-                name: this.config.name,
-                messageID: info.messageID,
-                author: event.senderID,
-                messID: event.messageID,
-                id: idbox,
-                type: "calladmin"
-              })
-            );
-          }
-          for (var b of s) {
-            fs.unlinkSync(b);
-          }
+      } else {
+        for (let ad of idad) {
+          api.sendMessage({
+            body: `📱 ===[CALL ADMIN]====📱\n\n\n[👤] Report from: ${name}\n[❗] ID User ${uid}\n[ 👧 👩 👧 👨 ] BOX: ${namethread}\n[🔰] ID BOX: ${idbox}\n\n[💌] Inbox: ${args.join(" ") || "only files that do not have report ❤️ content"}\n[⏰] Time: ${gio}\n[📌] Attach a file`,
+            attachment: l,
+            mentions: [{ id: event.senderID, tag: name }]
+          }, ad, (error, info) => global.client.handleReply.push({
+            name: this.config.name,
+            messageID: info.messageID,
+            author: event.senderID,
+            messID: event.messageID,
+            id: idbox,
+            type: "calladmin"
+          }));
         }
+        for (var b of s) fs.unlinkSync(b);
       }
-      , event.messageID);
-  }
-  catch (ex) {
+    }, event.messageID);
+  } catch (ex) {
     console.log(ex);
   }
 };
