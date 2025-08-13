1b1c9cc5e1b36831fc63622b7384e6c72139670d
Tamimkot2324 <mdsaim232456@gmail.com>
2025-08-13 22:38:07 +0600

    Update top.js

--- Rudra/commands/top.js
+++ Rudra/commands/top.js
@@ -1,107 +1,87 @@
 module.exports.config = {
-  name: "top",
-  version: "0.0.5",
-  hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-  description: "Top Server!",
-  commandCategory: "group",
-  usages: "[thread/user/money/level]",
-  cooldowns: 5
+    name: "top",
+    version: "0.0.6",
+    hasPermssion: 0,
+    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    description: "Top Server!",
+    commandCategory: "group",
+    usages: "[thread/user/money]",
+    cooldowns: 5
 };
 
 module.exports.run = async ({ event, api, args, Currencies, Users }) => {
     const { threadID, messageID } = event;
+    let option = parseInt(args[1]) || 10;
+    if (isNaN(option) || option <= 0) return api.sendMessage("List length must be a number greater than 0", threadID, messageID);
 
+    const fs = require("fs-extra");
 
-  ///////////////////////////////////////////
-  //===== Check if there is a limit or not =====//
-  if (args[1] && isNaN(args[1]) || parseInt(args[1]) <= 0) return api.sendMessage("list length information must be a number and not less than 0", event.threadID, event.messageID);
-  var option = parseInt(args[1] || 10);
-  var data, msg = "";
-
-  ///////////////////////////////////////
-  //===== Check the case =====//
-  var fs = require("fs-extra");
-  var request = require("request");  // Covernt exp to level
     function expToLevel(point) {
-  if (point < 0) return 0;
-  return Math.floor((Math.sqrt(1 + (4 * point) / 3) + 1) / 2);
+        if (point < 0) return 0;
+        return Math.floor((Math.sqrt(1 + (4 * point) / 3) + 1) / 2);
     }
-    //level 
-    if (args[0] == "user") { 
-    let all = await Currencies.getAll(['userID', 'exp']);
+
+    // ===== Top Users by Level =====
+    if (args[0] == "user") {
+        let all = await Currencies.getAll(['userID', 'exp']);
         all.sort((a, b) => b.exp - a.exp);
-        let num = 0;
-               let msg = {
-          body: 'The 10 People Highest Level On Server!',
-          
+
+        let msgBody = '🏆 Top Users by Level:\n';
+        for (let i = 0; i < Math.min(option, all.length); i++) {
+            let user = all[i];
+            let level = expToLevel(user.exp);
+            let userData = await Users.getData(user.userID);
+            msgBody += `${i + 1}. ${userData.name} - Level ${level}\n`;
         }
-        for (var i = 0; i < 10; i++) {
-           
-   
-          let level = expToLevel(all[i].exp);
-          var name = (await Users.getData(all[i].userID)).name;      
-  
-          num += 1;
-          msg.body += '\n' + num + '. ' + name + ' - level ' + level;}
-           console.log(msg.body)
-                    api.sendMessage(msg, event.threadID, event.messageID)
-    }
-  if (args[0] == "thread") {
-    var threadList = [];
-    
-    //////////////////////////////////////////////
-    //===== Get the entire group and message number =====//
-    try {
-          data = await api.getThreadList(option + 10, null, ["INBOX"]);
-    }
-    catch (e) {
-      console.log(e);
+
+        return api.sendMessage(msgBody, threadID, messageID);
     }
 
-    for (const thread of data) {
-      if (thread.isGroup == true) threadList.push({ threadName: thread.name, threadID: thread.threadID, messageCount: thread.messageCount });
-    }
-    
-    /////////////////////////////////////////////////////
-    //===== Sort from highest to lowest for each group =====//
-    threadList.sort((a, b) => {
-      if (a.messageCount > b.messageCount) return -1;
-            if (a.messageCount < b.messageCount) return 1;
-    })
+    // ===== Top Threads by Messages =====
+    if (args[0] == "thread") {
+        let threadList = [];
+        let data;
+        try {
+            data = await api.getThreadList(option + 10, null, ["INBOX"]);
+        } catch (e) {
+            console.log(e);
+            return api.sendMessage("Error fetching thread list.", threadID, messageID);
+        }
 
-    ///////////////////////////////////////////////////////////////
-    //===== Start getting the push list into the return template =====//
-    var i = 0;
-    for(const dataThread of threadList) {
-      if (i == option) break;
-      msg += `${i+1}/ ${(dataThread.threadName)||"No name"}\nTID: [${dataThread.threadID}]\nNumber of message: ${dataThread.messageCount} message\n\n`;
-      i+=1;
+        for (const thread of data) {
+            if (thread.isGroup) threadList.push({
+                threadName: thread.name,
+                threadID: thread.threadID,
+                messageCount: thread.messageCount
+            });
+        }
+
+        threadList.sort((a, b) => b.messageCount - a.messageCount);
+
+        let msgBody = `🏆 Top ${Math.min(option, threadList.length)} Groups by Messages:\n`;
+        for (let i = 0; i < Math.min(option, threadList.length); i++) {
+            let t = threadList[i];
+            msgBody += `${i + 1}. ${t.threadName || "No name"}\nTID: [${t.threadID}]\nMessages: ${t.messageCount}\n\n`;
+        }
+
+        return api.sendMessage(msgBody, threadID, messageID);
     }
-    
-    return api.sendMessage(`Top ${threadList.length} Groups Have The Most Number Of Message:\n_____________________________\n${msg}\n_____________________________`, threadID, messageID);
-  }
-  
- if (args[0] == "money") { 
-    let all = await Currencies.getAll(['userID', 'money']);
+
+    // ===== Top Users by Money =====
+    if (args[0] == "money") {
+        let all = await Currencies.getAll(['userID', 'money']);
         all.sort((a, b) => b.money - a.money);
-        let num = 0;
-               let msg = {
-          body: 'The 10 People Richest On Server!',
-          
+
+        let msgBody = '💰 Top Richest Users:\n';
+        for (let i = 0; i < Math.min(option, all.length); i++) {
+            let user = all[i];
+            let userData = await Users.getData(user.userID);
+            msgBody += `${i + 1}. ${userData.name} - ${user.money}💵\n`;
         }
-        for (var i = 0; i < 10; i++) {
-        
-   
-          let level = all[i].money;
-      
-          var name = (await Users.getData(all[i].userID)).name;    
-                    
-          num += 1;
-          msg.body += '\n' + num + '. ' + name + ': ' + level + "💵";}
-                    console.log(msg.body)
-                    api.sendMessage(msg, event.threadID, event.messageID)
+
+        return api.sendMessage(msgBody, threadID, messageID);
     }
 
-}
-
+    // ===== If no valid argument =====
+    return api.sendMessage("Please use: +top [user/thread/money]", threadID, messageID);
+};
