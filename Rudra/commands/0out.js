98ee4964ac791a0247aaafeee09d7d8fa0373d76
Mr Saim👀 <mdsaim232456@gmail.com>
2025-08-14 23:36:07 +0600

    Update 0out.js

--- Rudra/commands/0out.js
+++ Rudra/commands/0out.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "out",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝗠𝗱 𝗧𝗮𝗺𝗶𝗺",
     description: "",
     commandCategory: "Admin",
     usages: "out [id]",
