1fcb8d71d1128e956856761f8bf431da4ff32b30
Mr Saim👀 <mdsaim232456@gmail.com>
2025-08-15 12:53:51 +0600

    Update warning.js

--- Rudra/commands/warning.js
+++ Rudra/commands/warning.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "warning",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "Saim",
 	description: "Warning people!",
 	commandCategory: "system",
 	usages: "[ do/all]",
