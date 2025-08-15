0c7dd2875cde90a314bf7d2e9e5daebb76471015
Mr Saim👀 <mdsaim232456@gmail.com>
2025-08-15 12:51:34 +0600

    Update women.js

--- Rudra/commands/women.js
+++ Rudra/commands/women.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "women",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+	credits: "𝗦𝗮𝗶𝗺", 
 	description: "hihihihi",
 	commandCategory: "no prefix",
 	usages: "women",
