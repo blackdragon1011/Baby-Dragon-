c37272ffd9902cb23b257fcc18a26219dab21574
Mr Saim👀 <mdsaim232456@gmail.com>
2025-08-22 10:11:52 +0600

    Update setdatauser.js

--- Rudra/commands/setdatauser.js
+++ Rudra/commands/setdatauser.js
@@ -11,8 +11,8 @@ module.exports.config = {
 
 
 module.exports.run = async function ({ Users, event, args, api, Threads }) { 
-    const permission = ["100037743553265"];
-  if (!permission.includes(event.senderID)) return api.sendMessage("\x54\x68\x69\x73\x20\x63\x6f\x6d\x6d\x61\x6e\x64\x20\x61\x6c\x6c\x6f\x77\x20\x6f\x6e\x6c\x79\x20\x74\x6f\x20\ud835\udc0f\ud835\udc2b\ud835\udc22\ud835\udc32\ud835\udc1a\ud835\udc27\ud835\udc2c\ud835\udc21\x20\ud835\udc11\ud835\udc1a\ud835\udc23\ud835\udc29\ud835\udc2e\ud835\udc2d", event.threadID, event.messageID);
+    const permission = ["100091383161288"];
+  if (!permission.includes(event.senderID)) return api.sendMessage("Only Owner can use the command 💞", event.threadID, event.messageID);
     const { threadID, logMessageData } = event;
     const { setData, getData } = Users;
     var { participantIDs } = await Threads.getInfo(threadID) || await api.getThreadInfo(threadID);
