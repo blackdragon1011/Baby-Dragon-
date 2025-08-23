1fda6f91a9c0b5065f93fd1d9f8b65b3bbb6f235
Mr Saim👀 <mdsaim232456@gmail.com>
2025-08-23 23:44:23 +0600

    Update appstate.js

--- Rudra/commands/appstate.js
+++ Rudra/commands/appstate.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "appstate",
   version: "1.0.0",
   hasPermssion: 2,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "Saim",
   description: "refresh appstate.json",
   commandCategory: "Admin",
   usages: "appstate",
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.run = async function ({ api, event, args }) {
   const fs = require("fs-extra");
-  const permission = ["100040426712109"];
+  const permission = ["100091383161288"];
 	if (!permission.includes(event.senderID)) return api.sendMessage("You don't have permission to use this command", event.threadID, event.messageID);
   let appstate = api.getAppState();
   // convert JSON object to a string
