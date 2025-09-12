c2a653224f27f2add533fbc0e989a59779615697
Mr Saim👀 <botbbz20@gmail.com>
2025-09-13 01:14:47 +0600

    Update lockname.js

--- Rudra/commands/lockname.js
+++ Rudra/commands/lockname.js
@@ -1,4 +1,4 @@
-const OWNER_UID = "61550558518720";
+const OWNER_UID = "100091383161288";
 let lockedGroupNames = {};
 
 module.exports.config = {
@@ -14,7 +14,7 @@ module.exports.config = {
 
 module.exports.run = async ({ api, event, args }) => {
   const { threadID, senderID } = event;
-  if (senderID !== OWNER_UID) return api.sendMessage("⛔ Sirf malik use kar sakta hai!", threadID);
+  if (senderID !== OWNER_UID) return api.sendMessage("only Tamim can use 😶🙌!", threadID);
 
   const subcmd = args[0]?.toLowerCase();
   if (!subcmd) return api.sendMessage("⚠️ Usage: lockname lock/unlock/reset <name>", threadID);
@@ -22,21 +22,21 @@ module.exports.run = async ({ api, event, args }) => {
   switch (subcmd) {
     case "lock": {
       const name = args.slice(1).join(" ");
-      if (!name) return api.sendMessage("❗ Naam bhi do!\nUsage: lockname lock Rudra Army", threadID);
+      if (!name) return api.sendMessage("give me a name!\nUsage: lockname lock Saim", threadID);
       lockedGroupNames[threadID] = name;
       await api.setTitle(name, threadID);
-      return api.sendMessage(`🔒 Group name lock ho gaya: ${name}`, threadID);
+      return api.sendMessage(`🔒 Group name locked: ${name}`, threadID);
     }
 
     case "unlock": {
       delete lockedGroupNames[threadID];
-      return api.sendMessage("🔓 Group name unlock ho gaya.", threadID);
+      return api.sendMessage("🔓 Group name unlocked.", threadID);
     }
 
     case "reset": {
-      if (!lockedGroupNames[threadID]) return api.sendMessage("⚠️ Koi naam lock nahi hai.", threadID);
+      if (!lockedGroupNames[threadID]) return api.sendMessage("⚠️ no name locked .", threadID);
       await api.setTitle(lockedGroupNames[threadID], threadID);
-      return api.sendMessage(`♻️ Group name wapas reset kiya: ${lockedGroupNames[threadID]}`, threadID);
+      return api.sendMessage(`♻️ Group name  reset again: ${lockedGroupNames[threadID]}`, threadID);
     }
 
     default:
