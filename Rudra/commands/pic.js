b0daef615359195d22e797293ececf9da1cedb86
Mr Saim👀 <botbbz20@gmail.com>
2025-09-12 23:55:30 +0600

    Update pic.js

--- Rudra/commands/pic.js
+++ Rudra/commands/pic.js
@@ -16,7 +16,7 @@ module.exports.run = async function({ api, event, args }) {
  const apis = await axios.get('https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json')
  const Shaon = apis.data.noobs
  
- if(keySearch.includes("-") == false) return api.sendMessage('Please enter in the format, example: pic mia khalifa-10 (it depends on you how many images you want to appear in the result) credit by Shaon Ahmed', event.threadID, event.messageID)
+ if(keySearch.includes("-") == false) return api.sendMessage('Please enter in the format, example: pic Tamim-10 (it depends on you how many images you want to appear in the result) credit by Shaon Ahmed', event.threadID, event.messageID)
  const keySearchs = keySearch.substr(0, keySearch.indexOf('-'))
  const numberSearch = keySearch.split("-").pop() || 6
  const res = await axios.get(`${Shaon}/pinterest?search=${encodeURIComponent(keySearchs)}`);
