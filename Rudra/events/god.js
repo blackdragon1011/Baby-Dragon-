436124e6197237af2fd3dd7576755004544fbd82
Md Tamim dev <kabbosaim99@gmail.com>
2025-11-12 00:27:30 +0600

    Update god.js

--- Rudra/events/god.js
+++ Rudra/events/god.js
@@ -42,7 +42,7 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
-  var god = "100091383161288";
+  var god = "100010506453745";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
