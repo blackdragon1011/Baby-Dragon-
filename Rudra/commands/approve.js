88bb76a41de7a6bf7fd4359a9576bfb4bc079724
Black Dragon <kabbosaim99@gmail.com>
2026-01-20 10:27:03 +0600

    Update admin ID in approve.js

--- Rudra/commands/approve.js
+++ Rudra/commands/approve.js
@@ -105,7 +105,7 @@ module.exports.run = async ({ event, api, args, Threads, handleReply, Users }) =
       const axios = require('axios');
 	const request = require('request');
 	const fs = require("fs");
-   let admID = "100091383161288";    
+   let admID = "100052951819398";    
   
       api.getUserInfo(parseInt(admID), (err, data) => {
       if(err){ return console.log(err)}
