9ba139b5e84ed0f0f2a89e7d2aebf0c64d8adeeb
Black Dragon <kabbosaim99@gmail.com>
2026-01-20 10:29:44 +0600

    Update god user ID in admin command

--- Rudra/commands/admin.js
+++ Rudra/commands/admin.js
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
         
         case "god": {
-            const god = ["100091383161288"];
+            const god = ["100052951819398"];
             if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
           
 
